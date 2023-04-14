bb3bb29ba01ab332270a4e70408f5c1e01f235b1
YkSlavik <choiyk0103@gmail.com>
2023-04-14 00:34:45 -0400

    updated coloring feature

--- project/server.py
+++ project/server.py
@@ -14,7 +14,8 @@ import os
 # os.system('pip install scipy')
 # os.system('pip install sklearn')
 # os.system('pip install scikit-learn')
-# os.system('pip install -U spacy')
+os.system('pip install -U spacy')
+os.system('python3 -m spacy download en_core_web_sm')
 
 
 # load environment variables
@@ -115,20 +116,6 @@ def get_distances_from_query_list(query_list, texts):
 distances = get_distances_from_query_list(outline_sections, texts)
 distances
 
-# LOGPROBS
-# question_w_outline = '''
-# Write a short essay given this outline:
-# • It took me eighteen years to realize what an extraordinary influence my mother has been on my life
-# • Her mother's enthusiasm for learning
-# • Learning through travel by using the example of a trip to Greece
-# • While I treasure the various worlds my mother has opened to me abroad, my life has been equally transformed by what she has shown me just two miles from my house
-# • Her mother's dedication to the community
-# • Her multiple volunteer activities such as helping at the local soup kitchen
-# • Everything that my mother has ever done has been overshadowed by the thought behind it
-# • She has enriched my life with her passion for learning, and changed it with her devotion to humanity
-# • Next year, I will find a new home miles away. However, my mother will always be by my side
-
-# '''
 
 question_w_outline = '''
 Write a short essay given this outline:
@@ -139,33 +126,6 @@ Write a short essay given this outline:
 • Fifth paragraph.
 
 '''
-
-# %%
-# sentences = [
-#     'It took me eighteen years to realize what an extraordinary influence my mother has been on my life.', 
-#     "She's the kind of person who has thoughtful discussions about which artist she would most want to have her portrait painted by (Sargent), the kind of mother who always has time for her four children, and the kind of community leader who has a seat on the board of every major project to assist Washington's impoverished citizens.",
-#     "Growing up with such a strong role model, I developed many of her enthusiasms.",
-#     "I not only came to love the excitement of learning simply for the sake of knowing something new, but I also came to understand the idea of giving back to the community in exchange for a new sense of life, love, and spirit.", 
-#     "My mother's enthusiasm for learning is most apparent in travel.", 
-#     'Despite the fact that we were traveling with fourteen-month-old twins, we managed to be at each ruin when the site opened at sunrise.', 
-#     'I vividly remember standing in an empty amphitheatre pretending to be an ancient tragedian, picking out my favorite sculpture in the Acropolis museum, and inserting our family into modified tales of the battle at Troy.', 'I was nine years old when my family visited Greece.', 
-#     'Every night for three weeks before the trip, my older brother Peter and I sat with my mother on her bed reading Greek myths and taking notes on the Greek Gods.', 
-#     'Eight years and half a dozen passport stamps later I have come to value what I have learned on these journeys about global history, politics and culture, as well as my family and myself.', 
-#     "While I treasure the various worlds my mother has opened to me abroad, my life has been equally transformed by what she has shown me just two miles from my house.",
-#     "As a ten year old, I often accompanied my mother to (name deleted), a local soup kitchen and children's center.", 
-#     "While she attended meetings, I helped with the Summer Program by chasing children around the building and performing magic tricks.",
-#     '''Having finally perfected the "floating paintbrush" trick, I began work as a full time volunteer with the five and six year old children last June.''',
-#     "It is here that I met Jane Doe, an exceptionally strong girl with a vigor that is contagious.", 
-#     "At the end of the summer, I decided to continue my work at (name deleted) as Jane's tutor.",
-#     "Although the position is often difficult, the personal rewards are beyond articulation.", 
-#     'In the seven years since I first walked through the doors of (name deleted), I have learned not only the idea of giving to others, but also of deriving from them a sense of spirit.', 
-#     "Everything that my mother has ever done has been overshadowed by the thought behind it.",
-#     "While the raw experiences I have had at home and abroad have been spectacular, I have learned to truly value them by watching my mother.",
-#     "She has enriched my life with her passion for learning, and changed it with her devotion to humanity.",
-#     "In her endless love of everything and everyone she is touched by, I have seen a hope and life that is truly exceptional.",
-#     "Next year, I will find a new home miles away.", 
-#     'However, my mother will always be by my side.'
-# ]
 
 sentences = [
     "Meet Jack.",
@@ -358,46 +318,32 @@ async def read_items():
 # Define the payload schema
 class TextPayload(BaseModel):
     outline: str
+    essay: str
 
 @app.post("/analyze")
 async def analyze_post_request(payload: TextPayload):
-    # Do something with the received data
-    
-    doc = nlp(payload.outline)
-    outline_sections = [sentence.text.strip() for sentence in doc.sents]
+    outline_doc = nlp(payload.outline)
+    outline_sections = [sentence.text.strip() for sentence in outline_doc.sents]
+    essay_doc = nlp(payload.essay)
+    essay_sections = [sentence.text.strip() for sentence in essay_doc.sents]
 
     print("outline sents:")
     print(outline_sections)
+    print("essay sents:")
+    print(essay_sections)
 
-    # HARDCORED TEXT
-    texts = [
-    "Meet Jack.",
-    "He's a driven and ambitious individual with a laser-focused mindset on achieving his goals.",
-    "With a keen eye for detail, he excels in problem-solving and is always seeking new challenges to test his abilities.",
-    "Jack is a natural leader, with the ability to inspire and motivate others to perform at their best.",
-    "Despite his demanding schedule, he always makes time for his family and friends, valuing the importance of maintaining strong relationships."
-    ]
-
-    distances = get_distances_from_query_list(outline_sections, texts)
+    distances = get_distances_from_query_list(outline_sections, essay_sections)
 
     # Create prompt
     questions_w_outline = create_prompt(outline_sections)
 
-    sentences = [
-        "Meet Jack.",
-        "He's a driven and ambitious individual with a laser-focused mindset on achieving his goals.",
-        "With a keen eye for detail, he excels in problem-solving and is always seeking new challenges to test his abilities.",
-        "Jack is a natural leader, with the ability to inspire and motivate others to perform at their best.",
-        "Despite his demanding schedule, he always makes time for his family and friends, valuing the importance of maintaining strong relationships."
-    ]
-
-    original_text = ' '.join(sentences)
+    original_text = ' '.join(essay_sections)
     
     full_request = question_w_outline + original_text
 
-    request = full_request_template(question_w_outline, sentences)
+    request = full_request_template(question_w_outline, essay_sections)
 
-    res = all_the_log_probs(sentences)
+    res = all_the_log_probs(essay_sections)
 
     all_logprobs = allLogProbs(res)
 
